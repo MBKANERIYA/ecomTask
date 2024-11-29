9fb01c80da7e0cd277d1970a810410b48d674612
MBKANERIYA <mbkaneriya1998@gmail.com>
2024-11-29 12:52:47 +0530

    first commit

---
+++ server/routes/cart.route.js
@@ -0,0 +1,11 @@
+let express = require("express")
+const { cartController } = require("../controller")
+
+let route = express.Router()
+
+route.post("/createCart", cartController.addCart)
+route.get("/getCart", cartController.getCart)
+route.delete("/delete/:id", cartController.deleteFromCart)
+route.put("/update/:id", cartController.cartUpdate)
+
+module.exports = route
